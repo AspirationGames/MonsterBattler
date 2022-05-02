2ec4f749cb8d209dca31b795cf37161e2aa2f245
Adriano <apdiazasper@gmail.com>
2022-05-02 12:19:38 -0400

    Added method to save scene states. So that when trainer transitions between scenes unloaded scenes keep their states accuratly

--- Monster Battler/Assets/Scripts/SceneManagement/SceneDetails.cs
+++ Monster Battler/Assets/Scripts/SceneManagement/SceneDetails.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Linq;
 
 public class SceneDetails : MonoBehaviour
 {
     [SerializeField] List<SceneDetails> connectedScenes;
     public bool IsLoaded {get; private set;}
+
+    List<SavableEntity> savableEntities;
     private void OnTriggerEnter2D(Collider2D other) 
     {
         if(other.tag == "Player")
@@ -42,8 +45,19 @@ public class SceneDetails : MonoBehaviour
     {
         if(!IsLoaded)
             {
-                SceneManager.LoadSceneAsync(gameObject.name, LoadSceneMode.Additive); //remeber scene needs to be the same name as the trigger game object.
+                var operation = SceneManager.LoadSceneAsync(gameObject.name, LoadSceneMode.Additive); //remeber scene needs to be the same name as the trigger game object.
                 IsLoaded = true;
+                //we save the scene in a variable to determine if the scene loading has been completed. 
+                //This will prefent the script from completing other function that require scene loading to be completed first.
+                
+                //This lamda function will ensure that the scene is completely loaded before the savable entities are loaded.
+                operation.completed += (AsyncOperation op) =>
+                {
+                    savableEntities = GetSaveAbleEntitiesInScene();
+                    SavingSystem.i.RestoreEntityStates(savableEntities);
+                };
+
+                
             }
     }
 
@@ -51,8 +65,18 @@ public class SceneDetails : MonoBehaviour
     {
         if(IsLoaded)
             {
+                SavingSystem.i.CaptureEntityStates(savableEntities);
+
                 SceneManager.UnloadSceneAsync(gameObject.name); //remeber scene needs to be the same name as the trigger game object.
                 IsLoaded = false;
             }
+    }
+
+    List<SavableEntity> GetSaveAbleEntitiesInScene()
+    {
+        var currentScene = SceneManager.GetSceneByName(gameObject.name);
+        var savableEntities = FindObjectsOfType<SavableEntity>().Where(x => x.gameObject.scene == currentScene).ToList();
+
+        return savableEntities;
     }
 }
